b2b51316618a39aa10e1e9c7ae460f3cb4754c7f
midudzik <midudzik@d17-428-pc-20.ipa.ii.agh.edu.pl>
2024-03-17 10:14:38 +0100

    Add new dependencies snd ascii render

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,4 +1,9 @@
 package pl.edu.agh.mwo.hellomaven;
+import com.indvd00m.ascii.render.Render;
+import com.indvd00m.ascii.render.api.ICanvas;
+import com.indvd00m.ascii.render.api.IContextBuilder;
+import com.indvd00m.ascii.render.api.IRender;
+import com.indvd00m.ascii.render.elements.PseudoText;
 
 /**
  * Simple main application
@@ -6,7 +11,13 @@ package pl.edu.agh.mwo.hellomaven;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        IRender render = new Render();
+        IContextBuilder builder = render.newBuilder();
+        builder.width(120).height(20);
+        builder.element(new PseudoText("Hello World!"));
+        ICanvas canvas = render.render(builder.build());
+        String s = canvas.getText();
+        System.out.println(s);
     }
     
     public int getRandomNumber() {

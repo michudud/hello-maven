815e447e43de6d2fda59d041477b966d86877840
midudzik <midudzik@d17-428-pc-20.ipa.ii.agh.edu.pl>
2024-03-17 10:27:07 +0100

    fix document according to checkstyle rules

--- src/main/java/pl/edu/agh/mwo/hellomaven/App.java
+++ src/main/java/pl/edu/agh/mwo/hellomaven/App.java
@@ -1,19 +1,27 @@
 package pl.edu.agh.mwo.hellomaven;
+
 import com.indvd00m.ascii.render.Render;
 import com.indvd00m.ascii.render.api.ICanvas;
 import com.indvd00m.ascii.render.api.IContextBuilder;
 import com.indvd00m.ascii.render.api.IRender;
 import com.indvd00m.ascii.render.elements.PseudoText;
 
+import java.util.Random;
+
 /**
- * Simple main application
+ * Simple main application.
  */
 public class App {
 
+    public static final int textWidth = 120;
+    public static final int textHeight = 20;
+    public static final int diceSize = 6;
+
     public static void main(String[] args) {
+
         IRender render = new Render();
         IContextBuilder builder = render.newBuilder();
-        builder.width(120).height(20);
+        builder.width(textWidth).height(textHeight);
         builder.element(new PseudoText("Hello World!"));
         ICanvas canvas = render.render(builder.build());
         String s = canvas.getText();
@@ -22,7 +30,9 @@ public class App {
     
     public int getRandomNumber() {
         // chosen by fair dice roll.
-        // guaranteed to be random
-        return 4;
+        // guaranteed to be random.
+        Random rand = new Random();
+        int n = rand.nextInt(diceSize);
+        return n + 1;
     }
 }
